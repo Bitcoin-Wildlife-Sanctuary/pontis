af1ce17b25da896340b795941cbfa839c72723cc
harry <harry@example.com>
2025-01-20 11:45:41 +0800

    contract

---
+++ l1/src/contracts/aggregatorUtils.ts
@@ -0,0 +1,60 @@
+import { ByteString, hash256, len, method, OpCode, Sha256, sha256, SmartContractLib, toByteString } from "scrypt-ts";
+import { GeneralUtils } from "./generalUtils";
+
+
+export type AggregatorTransaction = {
+    ver: ByteString // version of the transaction
+    inputContract0: ByteString  // the first contract input, optional, if exists.
+    inputContract1: ByteString  // the second contract input, optional, if exists.
+    inputFee: ByteString // the fee input, required, also the last input of the transaction
+
+    outputContractAmt: bigint // the amount of the deposit aggregator contract output
+    outputContractSPK: ByteString // the script pubkey of the deposit aggregator contract output
+    hashData: Sha256 // Hash of state data, stored in OP_RETURN output.
+    locktime: ByteString // locktime of the transaction
+}
+
+export class AggregatorUtils extends SmartContractLib {
+
+    @method()
+    static getTxId(tx: AggregatorTransaction, isLeaf: boolean): Sha256 {
+        // we only have deposit aggregator
+        // leaf deposit tx: feeInput => aggregatorContractOutput + stateOutput
+        // non-leaf deposit tx: aggregatorContractInput + aggregatorContractInput + feeInput => aggregatorContractOutput + stateOutput
+
+        // todo: add an depositInfo opReturn output, which contains the deposit info, convinient for the indexer
+
+        const inputsPrefix = isLeaf ?
+            toByteString('01') :
+            (toByteString('03') + tx.inputContract0 + tx.inputContract1)
+        return hash256(
+            tx.ver +
+            inputsPrefix +
+            tx.inputFee +
+            toByteString('02') +
+            // todo: outputContractSPK may consist of length + scriptpubkey, this may be hard to check is the outputContractSPK is valid/allowed
+            GeneralUtils.getContractOutput(tx.outputContractAmt, tx.outputContractSPK) +
+            toByteString('000000000000000022') +    // opreturn output:  satoshis + length of script 
+            OpCode.OP_RETURN +
+            toByteString('20') +    
+            tx.hashData +   // todo: add some tag here which is useful for the indexer
+            tx.locktime
+        )
+    }
+
+    @method()
+    static getHashPrevouts(
+        txId0: Sha256,
+        txId1: Sha256,
+        feePrevout: ByteString
+    ): Sha256 {
+        return sha256(
+            txId0 +
+            toByteString('00000000') +
+            txId1 +
+            toByteString('00000000') +
+            feePrevout
+        )
+    }
+
+}
---
+++ l1/src/contracts/bridge.ts
@@ -0,0 +1,323 @@
+import {
+    assert,
+    hash256,
+    method,
+    sha256,
+    SmartContract,
+    toByteString,
+    Sig,
+    PubKey,
+    prop,
+    ByteString,
+    Sha256,
+} from 'scrypt-ts'
+import { SHPreimage, SigHashUtils } from './sigHashUtils'
+import { AggregatorTransaction, AggregatorUtils } from './aggregatorUtils'
+import {  MerklePath, MerkleProof } from './merklePath'
+import { GeneralUtils } from './generalUtils'
+
+
+export type BridgeTransaction = {
+    ver: ByteString     // version of the transaction
+    inputs: ByteString  // inputLengh + all inputs of the transaction
+
+    contractSPK: ByteString // the script pubkey of the bridge contract output
+    expanderSPK: ByteString // the script pubkey of the expander contract output
+
+    contractAmt: bigint // the amount of the bridge contract output
+    expanderAmt: bigint // the amount of the expander contract output
+    expanderStateHash: Sha256 // the state hash of the expander state output
+
+    // bridge contract state
+    batchesRoot: Sha256 // the hash of the batches, stored in OP_RETURN output
+    depositAggregatorSPK: ByteString     // Aggregator SPK's are separated from the script 
+    locktime: ByteString
+}
+
+export const MAX_NODES_AGGREGATED = 4
+export const MAX_INPUTS = 6;
+
+
+export class Bridge extends SmartContract {
+    @prop()
+    operator: PubKey
+
+    @prop()
+    expanderSPK: ByteString
+
+    constructor(
+        operator: PubKey,
+        expanderSPK: ByteString
+    ) {
+        super(...arguments)
+        this.operator = operator
+        this.expanderSPK = expanderSPK
+    }
+
+    @method()
+    public finalizeL1Deposit(
+        shPreimage: SHPreimage,
+        sigOperator: Sig,
+        prevTx: BridgeTransaction,           // Previous bridge update transaction.
+        aggregatorTx: AggregatorTransaction, // Root aggregator transaction.
+        fundingPrevout: ByteString,
+
+        batchHash: Sha256,
+        batchProof: MerkleProof
+    ) {
+        // Check sighash preimage.
+        const s = SigHashUtils.checkSHPreimage(shPreimage)
+        assert(this.checkSig(s, SigHashUtils.Gx))
+
+        // Check operator sig.
+        assert(this.checkSig(sigOperator, this.operator))
+
+        // Construct prev txids.
+        const prevTxId = this.getTxId(prevTx)
+        const aggregatorTxId = AggregatorUtils.getTxId(aggregatorTx, false)
+
+        // Check this transaction unlocks specified outputs in the correct order.
+        const hashPrevouts = Bridge.getHashDepositTxPrevouts(
+            prevTxId,
+            aggregatorTxId,
+            fundingPrevout
+        )
+        assert(hashPrevouts == shPreimage.hashPrevouts)
+
+        // Make sure this is unlocked via first input.
+        assert(shPreimage.inputNumber == toByteString('00000000'))
+
+        // Check second input unlocks correct aggregator script.
+        // todo: prevTx.depositAggregatorSPK can be forged
+        assert(prevTx.depositAggregatorSPK == aggregatorTx.outputContractSPK)
+
+        // Check that the previous leaf is null
+        assert(MerklePath.calcMerkleRoot(MerklePath.NULL_NODE, batchProof) == prevTx.batchesRoot)
+
+        // Construct new batches root
+        const batchesRootNew: Sha256 = MerklePath.calcMerkleRoot(
+            batchHash,
+            batchProof
+        )
+
+        // Create new contract output.
+        // Add total amount deposited to the bridge output balance.
+        const contractOut = GeneralUtils.getContractOutput(
+            prevTx.contractAmt + aggregatorTx.outputContractAmt,
+            prevTx.contractSPK
+        )
+
+        // Create state output with new state hash.
+        const stateHash = Bridge.getStateHash(
+            batchesRootNew, prevTx.depositAggregatorSPK
+        )
+        const conractStateOut = GeneralUtils.getStateOutput(stateHash)
+
+        // Enforce outputs.
+        const hashOutputs = sha256(
+            contractOut +
+            conractStateOut
+        )
+        assert(hashOutputs == shPreimage.hashOutputs)
+    }
+
+    @method()
+    public finalizeL2Deposit(
+        shPreimage: SHPreimage,
+        sigOperator: Sig,   
+        prevTx: BridgeTransaction,           // Previous bridge update transaction.
+        fundingPrevout: ByteString,
+
+        batchHash: Sha256,
+        batchProof: MerkleProof
+    ) {
+        // finalizeDepositTx: bridgeInput + feeInput => bridgeOutput + stateOutput
+
+        // Check sighash preimage.
+        const s = SigHashUtils.checkSHPreimage(shPreimage)
+        assert(this.checkSig(s, SigHashUtils.Gx))
+
+        // Check operator sig.
+        assert(this.checkSig(sigOperator, this.operator))
+
+        // Construct prev txids.
+        const prevTxId = this.getTxId(prevTx)
+
+        // Check this transaction unlocks specified outputs in the correct order.
+        const hashPrevouts = Bridge.getHashNonDepositTxPrevouts(
+            prevTxId,
+            fundingPrevout
+        )
+        assert(hashPrevouts == shPreimage.hashPrevouts)
+
+        // Make sure this is unlocked via first input.
+        assert(shPreimage.inputNumber == toByteString('00000000'))
+
+        // Check that the previous leaf
+        assert(MerklePath.calcMerkleRoot(batchHash, batchProof) == prevTx.batchesRoot)
+
+        // Construct new batches root, delete the batch leaf
+        const batchesRootNew: Sha256 = MerklePath.calcMerkleRoot(
+            MerklePath.NULL_NODE,
+            batchProof
+        )
+
+        // Create new contract output.
+        // Substract total amount deposited of the bridge output balance.
+        const contractOut = GeneralUtils.getContractOutput(
+            prevTx.contractAmt,
+            prevTx.contractSPK
+        )
+
+        // Create state output with new state hash.
+        const stateHash = Bridge.getStateHash(
+            batchesRootNew, prevTx.depositAggregatorSPK
+        )
+        const conractStateOut = GeneralUtils.getStateOutput(stateHash)  
+
+        // Enforce outputs.
+        const hashOutputs = sha256(
+            contractOut +
+            conractStateOut
+        )
+        assert(hashOutputs == shPreimage.hashOutputs)
+    }
+
+    @method()
+    public createWithdrawal(
+        shPreimage: SHPreimage,
+        sigOperator: Sig,
+        prevTx: BridgeTransaction,           // Previous bridge update transaction.
+        fundingPrevout: ByteString,
+
+        expanderRoot: Sha256,
+        sumAmt: bigint
+    ) {
+        // Check sighash preimage.
+        const s = SigHashUtils.checkSHPreimage(shPreimage)
+        assert(this.checkSig(s, SigHashUtils.Gx))
+
+        // Check operator sig.
+        assert(this.checkSig(sigOperator, this.operator))
+
+        // Construct prev txids.
+        const prevTxId = this.getTxId(prevTx)
+
+        // Check this transaction unlocks specified outputs in the correct order.
+        const hashPrevouts = Bridge.getHashNonDepositTxPrevouts(
+            prevTxId,
+            fundingPrevout
+        )
+        assert(hashPrevouts == shPreimage.hashPrevouts)
+
+        // Make sure this is unlocked via first input.
+        assert(shPreimage.inputNumber == toByteString('00000000'))
+
+        // Substract total amount withrawn of the bridge output balance.
+        const contractOut = GeneralUtils.getContractOutput(
+            prevTx.contractAmt - sumAmt,
+            prevTx.contractSPK
+        )
+
+        // Create state output with new state hash.
+        const bridgeStateHash = Bridge.getStateHash(
+            prevTx.batchesRoot, prevTx.depositAggregatorSPK
+        )
+        const expanderStateHash = sha256(expanderRoot);
+
+        const bridgeStateOut = GeneralUtils.getStateOutput(bridgeStateHash);
+
+        // Create an expander P2TR output which carries the total amount withrawn.
+        const expanderOut = GeneralUtils.getContractOutput(
+            sumAmt,
+            this.expanderSPK
+        )
+        const expanderStateOut = GeneralUtils.getStateOutput(expanderStateHash);
+
+        // Enforce outputs.
+        const hashOutputs = sha256(
+            contractOut +
+            bridgeStateOut +
+            expanderOut +
+            expanderStateOut
+        )
+        assert(hashOutputs == shPreimage.hashOutputs)
+    }
+
+
+    @method()
+    private getTxId(tx: BridgeTransaction): Sha256 {
+        // depositTx: bridgeInput + depositAggregatorInput => bridgeOutput + stateOutput
+        // withdrawTx: bridgeInput + feeInput => bridgeOutput + bridgeStateOutput + expanderOutput + expanderStateOutput;
+        // finalizeDepositTx: bridgeInput + feeInput => bridgeOutput + stateOutput
+        const nOutputs = tx.expanderAmt == 0n ? toByteString('02') : toByteString('04')
+
+        const stateHash = Bridge.getStateHash(
+            tx.batchesRoot, tx.depositAggregatorSPK
+        )
+
+        let expanderOut = toByteString('')
+        let expanderStateOut = toByteString('')
+        if (tx.expanderAmt != 0n) {
+            expanderOut = GeneralUtils.getContractOutput(tx.expanderAmt, tx.expanderSPK)
+            assert(tx.expanderSPK == this.expanderSPK)
+            expanderStateOut = GeneralUtils.getStateOutput(tx.expanderStateHash)
+        }
+
+        return hash256(
+            tx.ver +
+            tx.inputs +
+            nOutputs +
+            GeneralUtils.getContractOutput(tx.contractAmt, tx.contractSPK) +
+            GeneralUtils.getStateOutput(stateHash) +
+            expanderOut +
+            expanderStateOut +
+            tx.locktime
+        )
+    }
+
+    @method()
+    static getHashDepositTxPrevouts(
+        prevStateTxId: Sha256,
+        aggregatorTxId: Sha256,
+        feePrevout: ByteString
+    ): Sha256 {
+        return sha256(
+            prevStateTxId +
+            toByteString('00000000') +
+            aggregatorTxId +
+            toByteString('00000000') +
+            feePrevout
+        )
+    }
+
+    @method()
+    static getHashNonDepositTxPrevouts(
+        prevStateTxId: Sha256,
+        feePrevout: ByteString
+    ): Sha256 {
+        return sha256(
+            prevStateTxId +
+            toByteString('00000000') +
+            feePrevout
+        )
+    }
+
+    /**
+     * Creates bridge state hash, stored in an OP_RETURN output.
+     *
+     * @param batchesRoot - Merkle root of bridges batches tree.
+     * @param depositAggregatorSPK - Deposit aggregator SPK.
+     * @returns 
+     */
+    @method()
+    static getStateHash(
+        batchesRoot: Sha256,
+        depositAggregatorSPK: ByteString
+    ): Sha256 {
+        return hash256(
+            batchesRoot + depositAggregatorSPK
+        )
+    }
+
+}
