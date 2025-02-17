99dbcd365d892df43b26f4190a4d76373fc21659
klouskingsley <12490787+klouskingsley@users.noreply.github.com>
2025-02-17 20:52:02 +0800

    add l1 transaction for withdrawal

--- operator/src/l1/transactions.ts
+++ operator/src/l1/transactions.ts
@@ -1,4 +1,4 @@
-import { DepositBatch, L1Tx, L1TxId, L1TxStatus } from '../state';
+import { DepositBatch, L1Tx, L1TxId, L1TxStatus, WithdrawalBatch } from '../state';
 import { distinctUntilKeyChanged, from, interval, Observable, switchMap, takeWhile, tap } from 'rxjs';
 import * as l1Api from './api';
 import { createL1Provider, UNCONFIRMED_BLOCK_NUMBER } from './deps/l1Provider';
@@ -68,3 +68,72 @@ export async function finalizeBatch(batch: DepositBatch): Promise<L1Tx> {
     blockNumber: UNCONFIRMED_BLOCK_NUMBER,
   };
 }
+
+/// create the withdrawal expander, return the L1Tx
+export async function createWithdrwalExpander(batch: WithdrawalBatch): Promise<L1Tx> {
+  const txid = await l1Api.createWithdrawal(
+    env.operatorSigner,
+    env.l1Network,
+    env.createUtxoProvider(),
+    env.createChainProvider(),
+    createL1Provider(env.useRpc, env.rpcConfig, env.l1Network),
+    getFileOffChainDataProvider(),
+    env.l1FeeRate,
+    batch
+  )
+  return {
+    type: 'l1tx',
+    hash: txid,
+    status: 'UNCONFIRMED',
+    blockNumber: UNCONFIRMED_BLOCK_NUMBER,
+  };
+}
+
+/// check if the withdrawal batch is completed, do not need to call expandWithdrawal if it is completed
+export function isExpandedCompleted(batch: WithdrawalBatch): boolean {
+  return !l1Api.shouldDistribute(batch) && !l1Api.shouldExpand(batch);
+}
+
+/// expand the withdrawal batch, return the txids. In the inner implementation, it will call expand or distribute
+export async function expandWithdrawal(batch: WithdrawalBatch): Promise<L1Tx[]> {
+  if (isExpandedCompleted(batch)) {
+    throw new Error('withdrawal batch is completed');
+  }
+  if (l1Api.shouldExpand(batch)) {
+    const txids = await l1Api.expandLevelWithdrawals(
+      env.operatorSigner,
+      env.l1Network,
+      env.createUtxoProvider(),
+      env.createChainProvider(),
+      createL1Provider(env.useRpc, env.rpcConfig, env.l1Network),
+      getFileOffChainDataProvider(),
+      env.l1FeeRate,
+      batch
+    );
+    return txids.map(txid => ({
+      type: 'l1tx',
+      hash: txid,
+      status: 'UNCONFIRMED',
+      blockNumber: UNCONFIRMED_BLOCK_NUMBER,
+    }));
+  }
+  if (l1Api.shouldDistribute(batch)) {
+    const txids = await l1Api.distributeLevelWithdrawals(
+      env.operatorSigner,
+      env.l1Network,
+      env.createUtxoProvider(),
+      env.createChainProvider(),
+      createL1Provider(env.useRpc, env.rpcConfig, env.l1Network),
+      getFileOffChainDataProvider(),
+      env.l1FeeRate,
+      batch
+    );
+    return txids.map(txid => ({
+      type: 'l1tx',
+      hash: txid,
+      status: 'UNCONFIRMED',
+      blockNumber: UNCONFIRMED_BLOCK_NUMBER,
+    }));
+  }
+  throw new Error('no reach here')
+} 
