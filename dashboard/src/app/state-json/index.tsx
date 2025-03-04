6f62ebad7e800879fad451f654c6acd54c025e0a
Uğur Eren <contact@ugureren.net>
2025-03-04 14:44:14 +0300

    fix build error

--- dashboard/src/app/state-json/index.tsx
+++ dashboard/src/app/state-json/index.tsx
@@ -1,15 +1,19 @@
 'use client';
 
-import ReactJson from 'react-json-view';
+import dynamic from 'next/dynamic';
 import {useTheme} from 'styled-components';
 
 import {useAutoUpdateState} from '@/hooks';
 import {StateWithDate} from '@/types';
+
+const DynamicReactJson = dynamic(() => import('react-json-view'), {
+  ssr: false,
+});
 
 export default function Page({initialState}: {initialState: StateWithDate}) {
   const {state, lastUpdate} = useAutoUpdateState(initialState);
 
   const theme = useTheme();
 
-  return <ReactJson theme="chalk" style={{padding: theme.spacings.small}} src={{lastUpdate, state}} />;
+  return <DynamicReactJson theme="chalk" style={{padding: theme.spacings.small}} src={{lastUpdate, state}} />;
 }
